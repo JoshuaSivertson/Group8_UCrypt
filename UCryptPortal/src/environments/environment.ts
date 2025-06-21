8844fcca69e41a185db92e3b54fe125462929aa9
JoshuaSivertson <joshsiv@shaw.ca>
2025-06-21 15:47:17 -0700

    Changes to main repository to run with local MySQL database
    Note: Add your MySQL username and password to the cypto-back\src\main\resources\application.properties
    file

--- UCryptPortal/src/environments/environment.ts
+++ UCryptPortal/src/environments/environment.ts
@@ -1,14 +1,19 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+/*
 export const environment = {
   production: false,
   apiDomain: '20.108.185.182:8888',
   apiBaseUrl: 'http://20.108.185.182:8888/',
   apiUrl: 'http://20.108.185.182:8888/ucrypt/api/v1/',
 };
-
+*/
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:8080/api/v1/',
+  apiDomain : 'localhost:8080'
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
